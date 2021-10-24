059e1451c1227a1afd29d76e615e0f75a5ffd9e4
Alexander_Acosta <alexanderacosta@unach.cl>
2021-10-24 19:15:08 -0300

    Finalizacion del curso

--- main.py
+++ main.py
@@ -95,23 +95,26 @@ def show_person(
         None, min_length=1,
         max_length=50,
         title="Person Name",
-        description="This is the person name,  It's between 1 and 50 characters"
+        description="This is the person name,  It's between 1 and 50 characters",
+        example="Rocio"
         ),
     age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age, It's required"
+        description="This is the person age, It's required",
+        example="25"
         )
 ):
     return {name: age}
 
-# Validaciones PAth Paremeters
+# Validaciones Path Paremeters
 
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
         ...,
-        gt=0
+        gt=0,
+        example=123
     )
 ):
     return {person_id: "It exists!"}
@@ -124,7 +127,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
     location: Location = Body(...)
