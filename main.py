00e7fe78c5eaaa3b89bbc227a2a8ff66f60f15ce
Alexander_Acosta <alexanderacosta@unach.cl>
2021-10-24 16:14:14 -0300

    request_response_body

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
@@ -9,5 +26,5 @@ def home():
 # Request and Response Body
 
 @app.post("/person/new")
-def create_person():
-    pass
+def create_person(person: Person = Body(...)):
+    return person
